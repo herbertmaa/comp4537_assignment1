d69c69276c170680076e562e47a538fae209ce07
herbertma <hma49@sfu.ca>
2021-03-20 18:37:11 -0700

    fixed pathing

--- source/app.js
+++ source/app.js
@@ -2,11 +2,11 @@
 
 const express = require('express');
 const path = require('path');
-const dbConnectionRouter = require('../routes/dbConnection');
+const dbConnectionRouter = require('./routes/dbConnection');
 const app = express();
 
-const indexRouter = require('../routes/index');
-const comp4537Router = require('../routes/comp4537');
+const indexRouter = require('./routes/index');
+const comp4537Router = require('./routes/comp4537');
 
 app.disable('x-powered-by')
 app.use(express.static(path.join(__dirname, '../public')));
--- source/routes/dbConnection.js
+++ source/routes/dbConnection.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const sql = require('../source/db');
+const sql = require('../db');
 
 
 router.route('/read/getQuestions').get(async (req, res) => {
